fdfa2a0bf10274a50697c57d73a792d880c5a805
luis angel <luichtec@gmail.com>
2023-03-23 13:45:55 -0700

    rama1

---
+++ bin/juego_flor.dart
@@ -0,0 +1,5 @@
+import 'package:juego_flor/juego_flor.dart' as juego_flor;
+
+void main(List<String> arguments) {
+  print('Hello world: ${juego_flor.calculate()}!');
+}
---
+++ test/juego_flor_test.dart
@@ -0,0 +1,8 @@
+import 'package:juego_flor/juego_flor.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
